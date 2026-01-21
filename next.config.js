c8534f0063475bf6589150957d1047af1d6599d8
peteris.troksa <peteris.troksa@gmail.com>
2026-01-21 19:40:53 +0000

    uj

---
+++ next.config.js
@@ -0,0 +1,17 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback.fs = false;
+        }
+        return config;
+    },
+};
+
+module.exports = nextConfig;
